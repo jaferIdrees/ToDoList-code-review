91d927256b01ac38005f42dc775b8d46cb0ca735
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-07-14 14:24:06 -0300

    Add remove testing

--- test/index.test.js
+++ test/index.test.js
@@ -2,23 +2,37 @@
  * @jest-environment jsdom
  */
 
- import {addTaskToPage} from '../src/addTaskToPage.js';
- import {addTask} from '../src/pageFunc';
- import Task from '../src/Task.js';
+import { addTaskToPage } from '../src/addTaskToPage.js';
+import { addTask } from '../src/pageFunc';
+import Task from '../src/Task.js';
 
- test('Add one new item to the list', () => {
-   document.body.innerHTML =
-   '<ul>'+
-   '<div class="dragContainer">'+
-   '</div>'+
-   '</ul>';
-   let tasks = [
-   ]
- for (let i=0; i<5; i++) {
-   const newTask = new Task('test', i, false);
-   addTaskToPage(addTask(tasks,newTask.description));
- }
-   const list = document.querySelectorAll('.lItem');
-   //expect(addTaskToPage).toBeCalledTimes(1);
-   expect(list).toHaveLength(5);
- });
+describe('Test Add functionality', () => {
+  test('Add one new item to the list', () => {
+    document.body.innerHTML =
+      '<ul>' +
+      '<div class="dragContainer">' +
+      '</div>' +
+      '</ul>';
+
+    let tasks = [
+    ]
+
+    for (let i = 0; i < 5; i++) {
+      const newTask = new Task('test', i, false);
+      addTaskToPage(addTask(tasks, newTask.description));
+    }
+
+    const list = document.querySelectorAll('.lItem');
+
+    //expect(addTaskToPage).toBeCalledTimes(1);
+    expect(list).toHaveLength(5);
+  });
+})
+
+describe('Test Remove functionality', () => {
+  test('Remove one new item to the list', () => {
+    document.getElementById('0').parentElement.querySelector('trashCont').click()
+    const list = document.querySelectorAll('.lItem');
+    expect(list).toHaveLength(4);
+  });
+})
